02c4f74ec24bccd38e6057fecf84ea96b9d31cc6
arajas <arajas@taltech.ee>
2025-04-29 00:29:14 +0300

    added main page code, icons, fonts

--- html_proto/src/App.jsx
+++ html_proto/src/App.jsx
@@ -1,5 +1,7 @@
+import MainPage from "./components/MainPage.jsx";
+
 function App() {
-  return "";
+  return <MainPage />;
 }
 
 export default App;
---
+++ html_proto/src/components/mainPage.jsx
@@ -0,0 +1,108 @@
+import React from "react";
+import "./MainPage.scss";
+
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  Button,
+  Form,
+  Navbar,
+  InputGroup,
+} from "react-bootstrap";
+
+function MainPage() {
+  const posts = [
+    {
+      category: "HEAOLU",
+      username: "terapeut_mare_287",
+      description: "Parimad nipid stressi maandamiseks",
+      likes: 14,
+      time: "30min ago",
+      image: "https://picsum.photos/id/237/100/130",
+    },
+    {
+      category: "FINANTS",
+      username: "edukas_tudeng300",
+      description: "Kuidas tudengina raha säästa?",
+      likes: 25,
+      time: "1hr ago",
+      image: "https://picsum.photos/id/10/100/130",
+    },
+    {
+      category: "HEAOLU",
+      username: "und_pole_207",
+      description: "Magan nii halvasti, hea une nippe?",
+      likes: 53,
+      time: "2hr ago",
+      image: "https://picsum.photos/id/26/100/130",
+    },
+    {
+      category: "TEHNOLOOGIA",
+      username: "chatgpt_bff_1010",
+      description: "Kuidas AI-d kasutades koolis mitte vahele jääda?",
+      likes: 102,
+      time: "2.5hr ago",
+      image: "https://picsum.photos/id/23/100/130",
+    },
+  ];
+
+  return (
+    <div className="main-page">
+      <Navbar expand="lg" className="custom-navbar mb-4 p-3 rounded">
+        <Container>
+          <Navbar.Brand style={{ fontFamily: "Borel", fontSize: "40px" }}>
+            Teejuht
+          </Navbar.Brand>
+          <InputGroup style={{ maxWidth: "400px" }}>
+            <Form.Control placeholder="Otsi postitust" />
+          </InputGroup>
+          <Button className="custom-button mb-3">Logi sisse</Button>
+        </Container>
+      </Navbar>
+
+      <Container>
+        <Row className="g-4">
+          {posts.map((post, idx) => (
+            <Col key={idx} xs={12} sm={6} md={4}>
+              <Card className="post-card">
+                <Row className="g-0">
+                  <Col xs={4} md={4}>
+                    <Card.Img src={post.image} className="card-img" />
+                  </Col>
+                  <Col md={8}>
+                    <Card.Body className="card-body">
+                      <Card.Title className="card-title">
+                        {post.category}
+                      </Card.Title>
+                      <Card.Subtitle className="card-subtitle">
+                        {post.username}
+                      </Card.Subtitle>
+                      <Card.Text className="card-text">
+                        {post.description}
+                      </Card.Text>
+                      <div className="card-footer">
+                        <div>
+                          <i className="bi bi-hand-thumbs-up"></i> {post.likes}
+                        </div>
+                        <div>
+                          <i className="bi bi-clock"></i> {post.time}
+                        </div>
+                        <div>
+                          <i className="bi bi-bookmark"></i>
+                        </div>
+                      </div>
+                    </Card.Body>
+                  </Col>
+                </Row>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default MainPage;
