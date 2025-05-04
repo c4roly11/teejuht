4d7c83b152a4f0b5be1906cb884e55933c54b7d5
camart <camart@taltech.ee>
2025-05-04 15:14:13 +0300

    Kõiki postitusi saab kategoriseerida

--- html_proto/src/components/CategoryPage.jsx
+++ html_proto/src/components/CategoryPage.jsx
@@ -3,6 +3,45 @@ import { useParams } from "react-router-dom";
 import { Container, Row } from "react-bootstrap";
 import PostCard from "./PostCard";
 
+const DefaultPosts = [
+  {
+    id: 1,
+    category: "HEAOLU",
+    username: "terapeut_mare_287",
+    description: "Parimad nipid stressi maandamiseks",
+    likes: 14,
+    time: "30min ago",
+    image: "https://picsum.photos/id/237/100/130",
+  },
+  {
+    id: 2,
+    category: "FINANTS",
+    username: "edukas_tudeng300",
+    description: "Kuidas tudengina raha säästa?",
+    likes: 25,
+    time: "1hr ago",
+    image: "https://picsum.photos/id/10/100/130",
+  },
+  {
+    id: 3,
+    category: "HEAOLU",
+    username: "und_pole_207",
+    description: "Magan nii halvasti, hea une nippe?",
+    likes: 53,
+    time: "2hr ago",
+    image: "https://picsum.photos/id/26/100/130",
+  },
+  {
+    id: 4,
+    category: "TEHNOLOOGIA",
+    username: "chatgpt_bff_1010",
+    description: "Kuidas AI-d kasutades koolis mitte vahele jääda?",
+    likes: 102,
+    time: "2.5hr ago",
+    image: "https://picsum.photos/id/23/100/130",
+  },
+];
+
 function CategoryPage() {
   const { categoryName } = useParams();
 
@@ -16,7 +55,9 @@ function CategoryPage() {
     console.warn("Error parsing posts from localStorage:", e);
   }
 
-  const filtered = posts.filter(
+  const allPosts = [...posts, ...DefaultPosts];
+
+  const filtered = allPosts.filter(
     (post) =>
       post &&
       post.category &&
@@ -32,7 +73,7 @@ function CategoryPage() {
       {filtered.length > 0 ? (
         <Row className="g-4">
           {filtered.map((post, idx) => (
-            <PostCard key={idx} post={post} index={idx} />
+            <PostCard key={idx} post={post} index={post.id} />
           ))}
         </Row>
       ) : (
